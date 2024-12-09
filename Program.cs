e3752b684c1f01bab61233378a4213a0bcd08330
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2024-12-09 20:50:35 -0300

    Update program entry point

--- Program.cs
+++ Program.cs
@@ -1,38 +1,43 @@
 using AuthSystem.Data;
 using AuthSystem.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using AuthSystem.Helper;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IContactRepository, ContactRepository>();
-
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowLocalhost",
-        builder => builder.WithOrigins("http://localhost:5210") 
+        builder => builder.WithOrigins("http://localhost:5210")
                           .AllowAnyHeader()
                           .AllowAnyMethod()
-                          .AllowCredentials()); 
+                          .AllowCredentials());
 });
 
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<ISection, Section>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession(o =>
+{
+    o.Cookie.HttpOnly = true; 
+    o.Cookie.IsEssential = true; 
+});
 
 var app = builder.Build();
-
 
 app.UseRouting();
 app.UseStaticFiles();
 app.UseAuthorization();
+app.UseSession();
 app.UseCors("AllowLocalhost");
-
 
 app.MapControllerRoute(
     name: "default",
@@ -41,7 +46,5 @@ app.MapControllerRoute(
 app.MapControllerRoute(
     name: "home",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
 
 app.Run();
