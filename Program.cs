a91fc7322dc8baa89c9acc741c60c78372cfd750
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2024-12-09 15:56:09 -0300

    Feat: Add and update controllers for login functionality.

--- Program.cs
+++ Program.cs
@@ -1,39 +1,47 @@
-using AuthSystem.Repository;
 using AuthSystem.Data;
+using AuthSystem.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IContactRepository, ContactRepository>();
+
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost",
+        builder => builder.WithOrigins("http://localhost:5210") 
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials()); 
+});
+
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+
 app.UseRouting();
 app.UseStaticFiles();
 app.UseAuthorization();
+app.UseCors("AllowLocalhost");
+
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
-    name: "contactDelete",
-    pattern: "contact/delete/{id}",
-    defaults: new { controller = "Contact", action = "Delete" });
-
-app.MapControllerRoute(
-    name: "contactDeleteConfirmation",
-    pattern: "contact/delete/confirm/{id}",
-    defaults: new { controller = "Contact", action = "DeleteConfirmation" });
-
-app.MapControllerRoute(
     name: "home",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();
