21869a94b2c220fd5a9d876c4b3fdd20ec6c7677
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2024-12-10 16:14:08 -0300

    Filter to admin

---
+++ Filters/RestrictPageOnlyAdmin.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using AuthSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AuthSystem.Filters
+{
+    public class RestrictPageOnlyAdmin : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+
+            string userSection = context.HttpContext.Session.GetString("UserLoggedSection");
+            if(string.IsNullOrEmpty(userSection))
+            {
+
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary{ {"controller","Login" },{"action","Index" } });
+
+            }
+            else
+            {
+
+                    UserModel user = JsonSerializer.Deserialize<UserModel>(userSection);
+
+                if(user == null)
+                {
+
+                 context.Result = new RedirectToRouteResult(new RouteValueDictionary{ {"controller","Login" },{"action","Index" } });
+
+                }
+                if(user.Profile != Enums.EnumProfile.Admin)
+                {
+                 context.Result = new RedirectToRouteResult(new RouteValueDictionary{ {"controller","Restrict" },{"action","Index" } });
+
+                }
+
+            }
+
+
+
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
