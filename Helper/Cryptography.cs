7f640124c908bd31f517af7ad89b2a2f45f63f32
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2024-12-10 16:13:39 -0300

    Cryptography System

---
+++ Helper/Cryptography.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Helper
+{
+    public static class Cryptography
+    {
+        public static string GenerateHash(this string value)
+        {
+
+            var hash = SHA1.Create();
+            var enconding = new ASCIIEncoding();
+            var array = enconding.GetBytes(value);
+
+            array = hash.ComputeHash(array);
+
+            var strHexa = new StringBuilder();
+
+            foreach(var item in array)
+            {
+                strHexa.Append(item.ToString("x2"));
+            }
+            return strHexa.ToString();
+
+        }
+    }
+}
