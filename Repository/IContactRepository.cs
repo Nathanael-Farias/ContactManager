b788764c59fe361b04d1282f58a5b900cddf4689
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2024-12-18 17:47:21 -0300

    Update IContactRepository Interface to Include User Filtering

--- Repository/IContactRepository.cs
+++ Repository/IContactRepository.cs
@@ -5,7 +5,7 @@ namespace AuthSystem.Repository
     public interface IContactRepository
     {
         Task<ContactModel> AddContactAsync(ContactModel contact);
-        Task<IEnumerable<ContactModel>> GetAllContactsAsync();
+        Task<IEnumerable<ContactModel>> GetAllContactsAsync(int userId);
         Task<ContactModel> GetContactByIdAsync(int id);
         Task<ContactModel> UpdateContactAsync(ContactModel contact);
         Task<bool> DeleteContactAsync(int id);
