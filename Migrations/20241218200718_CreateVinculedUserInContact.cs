ab729b4cca66f353bcd1a75f6be7a6dc5b18004e
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2024-12-18 17:45:37 -0300

    Update Model Snapshot for User Contact Binding

---
+++ Migrations/20241218200718_CreateVinculedUserInContact.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateVinculedUserInContact : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Contacts",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_UserId",
+                table: "Contacts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Users_UserId",
+                table: "Contacts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Users_UserId",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_UserId",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Contacts");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "id");
+        }
+    }
+}
