5a9c232927840cda810eb4a1e51add77e65125c4
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2024-12-17 22:26:12 -0300

    Add change password

--- Repository/IUserRepository.cs
+++ Repository/IUserRepository.cs
@@ -5,14 +5,15 @@ using AuthSystem.Models;
 namespace AuthSystem.Repository
 {
     public interface IUserRepository
-    {
+{
+    UserModel SearchByLogin(string login);
+    UserModel SearchByEmailAndLogin(string email, string login);
+    Task<UserModel> ChangePasswordAsync(ChangePasswordModel changePasswordModel); 
+    Task<UserModel> AddUserAsync(UserModel user);
+    Task<IEnumerable<UserModel>> GetAllUsersAsync();
+    Task<UserModel> GetUserByIdAsync(int id);
+    Task<UserModel> UpdateUserAsync(UserModel user);
+    Task<bool> DeleteUserAsync(int id);
+}
 
-        UserModel SearchByLogin(string login);
-        UserModel SearchByEmailAndLogin(string email, string login);
-        Task<UserModel> AddUserAsync(UserModel user);
-        Task<IEnumerable<UserModel>> GetAllUsersAsync();
-        Task<UserModel> GetUserByIdAsync(int id);
-        Task<UserModel> UpdateUserAsync(UserModel user);
-        Task<bool> DeleteUserAsync(int id);
-    }
 }
