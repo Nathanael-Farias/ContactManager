866d54918b41b31924accfc6845990723391527c
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2024-12-18 17:46:15 -0300

    Add UserId Property to Contact Model

--- Models/ContactModel.cs
+++ Models/ContactModel.cs
@@ -6,5 +6,8 @@ namespace AuthSystem.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        public int? UserId { get; set; }
+        public UserModel User { get; set; }
     }
 }
