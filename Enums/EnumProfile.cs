d73cbf2c3c4b863fa4828cf5a4eab63679a7b360
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-07 19:38:32 -0300

    Remove ignored files and clean cache

--- Enums/EnumProfile.cs
+++ Enums/EnumProfile.cs
@@ -3,7 +3,10 @@ namespace AuthSystem.Enums
     public enum EnumProfile
     {
         Admin = 1,
-        Default = 2
+        Default = 2,
+
+        DemoDefault = 3,
+        DemoAdmin = 4
 
     }
 }
